a0bdd3920d246333d2203ced516b6f7b97ad8808
Felipe Bona <felipechiodinibona@hotmail.com>
2024-07-24 22:10:09 -0300

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+    }
+
+}
