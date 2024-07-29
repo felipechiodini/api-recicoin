3c562c5016e19545b63a0996726b371c918442a0
Felipe Bona <felipechiodinibona@hotmail.com>
2024-07-29 02:17:29 -0300

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,27 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
 
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $credentials = request(['email', 'password']);
+        $loginUserData = $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ]);
 
-        if (Auth::attempt($credentials)) {
-            $user = User::query()
-                ->where('email', $credentials['email'])
-                ->first();
+        $user = User::query()
+            ->where('email', $loginUserData['email'])
+            ->first();
 
-            $token = $user->createToken('authToken');
+        if (!$user || !Hash::check($loginUserData['password'],$user->password)) {
+            return response()->json(['message' => 'Invalid Credentials'], 401);
         }
 
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+
         return response()
-            ->json([
-                'token' => $token->plainTextToken
-            ]);
+            ->json(compact('user', 'token'));
     }
 
 }
