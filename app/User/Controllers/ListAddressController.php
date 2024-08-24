e501c433ece69d92e145e0c28545e386008e34b0
Felipe Bona <felipechiodinibona@hotmail.com>
2024-08-24 11:25:00 -0300

    ajusts

--- app/User/Controllers/ListAddressController.php
+++ app/User/Controllers/ListAddressController.php
@@ -14,6 +14,7 @@ class ListAddressController
             ->get()
             ->map(function(UserAddress $userAddress) {
                 return [
+                    'id' => $userAddress->id,
                     'cep' => $userAddress->cep,
                     'street' => $userAddress->street,
                     'number' => $userAddress->number,
